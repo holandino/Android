1e3299660e6749b7d3fa43e31c823da4d77b40c2
Paulo Sergio <holandino@gmail.com>
2016-12-14 23:34:02 -0200

    Primeiro commit

---
+++ app/src/main/java/com/sabergio/myfirstapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.sabergio.myfirstapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Chamado quando usuário clica no botão.
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+        //TODO Fazer a segunda Activity
+    }
+
+}
